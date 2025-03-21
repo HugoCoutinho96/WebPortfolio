b0889b11cd70f75016d198750bd9f3201987760b
Hugo Coutinho <hugocoutinho96@outlook.com>
2025-03-21 10:50:29 -0300

    feat(frontend): versão inicial do projeto

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+
+const fonte = Montserrat({
+  subsets: ['latin']
+})
+
+export const metadata: Metadata = {
+  title: "Portfólio",
+  description: "Portfólio de projetos de desenvolvimento",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt">
+      <body
+        className={`${fonte.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
